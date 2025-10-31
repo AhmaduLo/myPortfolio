5caa25ecac673ab0ae07303b28a95b3e4736b033
AhmaduLo <ahmadubambagayelo@gmail.com>
2025-10-31 13:44:28 +0100

    supprimer les environnement sur gitignore

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  emailjs: {
+    serviceID: 'service_edtuyno',
+    templateID: 'template_p4t8lp7',
+    publicKey: 'G-EJy_SM-RxN2iHjf'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  emailjs: {
+    serviceID: 'service_edtuyno',
+    templateID: 'template_p4t8lp7',
+    publicKey: 'G-EJy_SM-RxN2iHjf'
+  }
+};
